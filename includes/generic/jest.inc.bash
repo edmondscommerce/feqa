36166f2a0cc371cc265b34fad4866b3e25a64c1a
dan <dan@dan>
2019-05-21 14:11:27 +0100

    Jest working now, tests passing

--- includes/generic/jest.inc.bash
+++ includes/generic/jest.inc.bash
@@ -6,8 +6,11 @@ while (( $jestExitCode > 0 ))
 do
     # Run command
     set -x;
-    $projectRoot/node_modules/.bin/jest
+    $projectRoot/node_modules/.bin/jest \
+    --rootDir=$projectRoot
+    --config=$jestConfigPath
     set +x;
+
     jestExitCode=$?
     if (( $jestExitCode > 0 ))
     then
