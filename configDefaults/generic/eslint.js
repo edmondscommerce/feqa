98200bfc3f3da1e349bf6fdd92da170066129725
dan <dan@dan>
2019-05-21 10:12:18 +0100

    Added config for TSLinting

--- configDefaults/generic/eslint.js
+++ configDefaults/generic/eslint.js
@@ -3,7 +3,9 @@ module.exports = {
     'browser': true,
     'es6': true
   },
+  'parser':  '@typescript-eslint/parser',
   'extends': [
+      'plugin:@typescript-eslint/recommended',
       'plugin:prettier/recommended',
       'plugin:vue/recommended'
   ],
